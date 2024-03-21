f6cb0a59dfc6ef87e3b44fbfc12e1fa5380dda85
Isaac Mayodi <mayodisaac3@gmail.com>
2024-03-21 09:31:38 +0300

    refined my code to reduce complexity, increase efficiency and reduce redudncy

--- engine.js
+++ engine.js
@@ -1,81 +1,69 @@
 import Game from "./scripts/game.js";
 
-window.addEventListener("load", function () {
-  this.document.getElementById("loading").style.display = "none";
+window.addEventListener("load", () => {
+  // Constants
+  const BLOCK = "block";
+  const NONE = "none";
+  const GRID = "grid";
+  // Loading screen ended
+  loading.style.display = NONE;
   // Canvas setup
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "white";
+  ctx.font = "30px Impact";
   resizeCanvas();
   function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   }
   window.addEventListener("resize", resizeCanvas);
-  ctx.fillStyle = "white";
-  ctx.strokeStyle = "white";
-  ctx.font = "30px Impact";
   // Other declarations
   const game = new Game(ctx, canvas);
-  const levelButtons = document.querySelector(".level-buttons");
   let gamePaused = false;
-  const controls = document.querySelector(".controls");
-
-  // const audio = document.getElementById("audio");
 
   function handlePage1() {
-    const firstPage = document.getElementById("firstPage");
-    play.addEventListener("click", () => {
-      click.play();
-      firstPage.style.display = "none";
-      document.getElementById("largeContainer").style.display = "block";
+    firstPage.addEventListener("click", (event) => {
+      if (event.target.id === "play") {
+        click.play();
+        firstPage.style.display = NONE;
+        largeContainer.style.display = BLOCK;
+      }
     });
   }
+
   function handlePage2() {
-    const levelButton = document.getElementById("levelButton");
     levelButton.addEventListener("click", () => {
       click.play();
-      levelButtons.style.display = "grid";
-      document.getElementById("largeContainer").style.display = "none";
-      document.getElementById("gameLogo").style.display = "none";
+      document.querySelector(".level-buttons").style.display = GRID;
+      largeContainer.style.display = NONE;
+      gameLogo.style.display = NONE;
     });
   }
   function handlePage3() {
-    const level1Button = document.getElementById("level1Button");
-    const level2Button = document.getElementById("level2Button");
-    const level3Button = document.getElementById("level3Button");
-    const level4Button = document.getElementById("level4Button");
-    const level5Button = document.getElementById("level5Button");
-    const level6Button = document.getElementById("level6Button");
-    const level7Button = document.getElementById("level7Button");
-    // Level buttons event listeners
-    level1Button.addEventListener("click", () => startGame(1));
-    level2Button.addEventListener("click", () => startGame(2));
-    level3Button.addEventListener("click", () => startGame(3));
-    level4Button.addEventListener("click", () => startGame(4));
-    level5Button.addEventListener("click", () => startGame(5));
-    level6Button.addEventListener("click", () => startGame(6));
-    level7Button.addEventListener("click", () => startGame(7));
+    document.querySelectorAll(".level-buttons img").forEach((img, index) => {
+      img.addEventListener("click", () => startGame(index + 1));
+    });
 
-    const counter = document.getElementById("counter");
-    const audioCounter = document.getElementById("audioCounter");
     const audio = new Audio("assets/sounds/background.mp3");
     audio.play();
     audio.loop = true;
     function startGame(level) {
       click.play();
-      levelButtons.style.display = "none";
-      canvas.style.display = "none";
-      controls.style.display = "none";
-      counter.style.display = "block";
+      document.querySelector(".level-buttons").style.display = NONE;
+      canvas.style.display = NONE;
+      document.querySelector(".controls").style.display = NONE;
+      counter.style.display = BLOCK;
       audio.pause();
       audioCounter.play();
       setTimeout(() => {
         // PAGE 4
         game.gameStarted = true;
         game.newGame.play();
-        counter.style.display = "none";
-        canvas.style.display = "block";
-        controls.style.display = "block";
+        counter.style.display = NONE;
+        canvas.style.display = BLOCK;
+        document.querySelector(".controls").style.display = BLOCK;
         audioCounter.pause();
         game.level = level;
         game.restart();
@@ -91,21 +79,21 @@ window.addEventListener("load", function () {
     }
   }
 
-  document.getElementById("pause").addEventListener("click", () => {
+  pause.addEventListener("click", () => {
     click.play();
-    document.getElementById("pause").style.display = "none";
-    menubuttons.style.display = "block";
-    exitMain.style.display = "block";
-    document.getElementById("playbutton").style.display = "block";
+    pause.style.display = NONE;
+    menubuttons.style.display = BLOCK;
+    exitMain.style.display = BLOCK;
+    playbutton.style.display = BLOCK;
     gamePaused = true;
     if (!gamePaused) requestAnimationFrame(animate);
   });
-  document.getElementById("playbutton").addEventListener("click", () => {
+  playbutton.addEventListener("click", () => {
     click.play();
-    document.getElementById("playbutton").style.display = "none";
-    menubuttons.style.display = "none";
-    exitMain.style.display = "none";
-    document.getElementById("pause").style.display = "block";
+    playbutton.style.display = NONE;
+    menubuttons.style.display = NONE;
+    exitMain.style.display = NONE;
+    pause.style.display = BLOCK;
     gamePaused = false;
     if (!gamePaused) requestAnimationFrame(animate);
   });
@@ -117,19 +105,16 @@ window.addEventListener("load", function () {
   // PAGE 3
   handlePage3();
 
-  function refreshPage() {
-    window.location.reload();
-  }
-  menubuttons.addEventListener("click", refreshPage);
+  menubuttons.addEventListener("click", () => window.location.reload());
   exitMain.addEventListener("click", () => {
     window.location.href = "notification.html";
-    this.window.open("", "_self", "");
-    this.window.close();
+    window.open("", "_self", "");
+    window.close();
   });
   exitLevel.addEventListener("click", () => {
     window.location.href = "notification.html";
-    this.window.open("", "_self", "");
-    this.window.close();
+    window.open("", "_self", "");
+    window.close();
   });
 
   // Animation Loop
